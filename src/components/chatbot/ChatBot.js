b75ba692f36979a5fa3cfd47dc5bcb7ffff35606
Adithya Krishna <aadithya794@gmail.com>
2021-09-24 14:31:35 +0530

    Added ChatBot
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- src/components/chatbot/ChatBot.js
+++ src/components/chatbot/ChatBot.js
@@ -1,3 +1,16 @@
 import ChatBot from 'react-simple-chatbot';
 
-export default ChatBot;
+function ChatBotApp() {
+    return(
+        <ChatBot
+            steps={[
+                {
+                    id: 'hello-world',
+                    message: 'Hello World!',
+                    end: true,
+                },
+            ]}
+        />
+    );
+};
+export default ChatBotApp;
