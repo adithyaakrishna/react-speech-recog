6f2e286ca699a318692bcde69bc83f5cfdff1a95
Adithya Krishna <aadithya794@gmail.com>
2021-09-24 14:49:18 +0530

    Added Form
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import ChatBot from './components/chatbot/ChatBot';
+import ExampleDBPedia from './components/RHSearch/RHSearch';
+import SimpleForm from './components/SimpleForm/SimpleForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ChatBot></ChatBot>
+        {/* <ChatBot></ChatBot> */}
+        <SimpleForm></SimpleForm>
+        <ExampleDBPedia></ExampleDBPedia>
       </header>
     </div>
   );
--- src/components/chatbot/ChatBot.js
+++ src/components/chatbot/ChatBot.js
@@ -1,16 +1,43 @@
 import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+    background: '#f5f8fb',
+    fontFamily: 'Helvetica Neue',
+    headerBgColor: '#EF6C00',
+    headerFontColor: '#fff',
+    headerFontSize: '15px',
+    botBubbleColor: '#EF6C00',
+    botFontColor: '#fff',
+    userBubbleColor: '#fff',
+    userFontColor: '#4a4a4a',
+};
 
 function ChatBotApp() {
     return(
-        <ChatBot
-            steps={[
-                {
-                    id: 'hello-world',
-                    message: 'Hello World!',
-                    end: true,
-                },
-            ]}
-        />
+        <ThemeProvider theme={theme}>
+            <ChatBot
+                headerTitle="Speech Recognition"
+                recognitionEnable={true}
+                steps={[
+                    {
+                        id: '1',
+                        message: 'What is your name?',
+                        trigger: '2',
+                    },
+                    {
+                        id: '2',
+                        user: true,
+                        trigger: '3',
+                    },
+                    {
+                        id: '3',
+                        message: 'Hi {previousValue}, nice to meet you!',
+                        end: true,
+                    },
+                ]}
+            />
+        </ThemeProvider>
     );
 };
 export default ChatBotApp;
