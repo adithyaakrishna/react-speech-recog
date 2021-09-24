1ec96908553805fac099514512c1681c0ad996bc
Adithya Krishna <aadithya794@gmail.com>
2021-09-24 13:47:03 +0530

    Added Files
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ChatBot from './components/chatbot/ChatBot';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ChatBot></ChatBot>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/chatbot/ChatBot.js
@@ -0,0 +1,3 @@
+import ChatBot from 'react-simple-chatbot';
+
+export default ChatBot;
